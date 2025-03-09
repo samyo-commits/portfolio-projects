d5860d97fd2b5a7acce02f10c5c8244983f36ed0
unknown <aherezasamuel19@gmail.com>
2025-03-09 23:47:47 +0300

    Initial commit

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+    
+    $to = "aherezasamuel19@gmail.com"; 
+    $subject = "New Contact Form Message";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again later.";
+    }
+}
+?>
